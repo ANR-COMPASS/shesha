9f724dcd9fa4eea3ffa7f4797733a1007aaa4663
Arnaud Sevin <Arnaud.Sevin@obspm.fr>
2020-09-24 10:42:08 +0200

    update check_shesha_compass_versions

--- shesha/__init__.py
+++ shesha/__init__.py
@@ -9,9 +9,12 @@ import subprocess, sys
 __version__ = "5.0.0"
 
 def check_shesha_compass_versions():
-    compass_version = subprocess.check_output('conda list compass | tail -n1',shell=True).decode(
-            sys.stdout.encoding).split()[1]
-    assert(__version__ == compass_version), 'SHESHA and COMPASS versions are not matching : %r != %r ' %\
+    compass_package = subprocess.check_output('conda list compass | tail -n1',shell=True).decode(
+            sys.stdout.encoding)
+    if(compass_package.startswith('compass')):
+        # using conda package version
+        compass_version = compass_package.split()[1]
+        assert(__version__ == compass_version), 'SHESHA and COMPASS versions are not matching : %r != %r ' %\
                                               (__version__, compass_version)
 
 check_shesha_compass_versions()
