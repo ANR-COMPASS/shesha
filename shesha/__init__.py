bc105e4845801a43a5ca27dbfd062d902c452f8a
Arnaud Sevin <Arnaud.Sevin@obspm.fr>
2023-12-14 12:26:33 +0100

    Update dependencies and check SHESHA and COMPASS versions

--- shesha/__init__.py
+++ shesha/__init__.py
@@ -4,8 +4,10 @@ Documentation for shesha.
 More details.
 '''
 
-import subprocess
 import sys
+import subprocess
+import rich
+from packaging import version
 
 __version__ = "5.5.0"
 
@@ -16,8 +18,18 @@ def check_shesha_compass_versions():
             sys.stdout.encoding)
     if(compass_package.startswith('compass')):
         # using conda package version
-        compass_version = compass_package.split()[1]
-        assert(__api_version__ == compass_version), 'SHESHA and COMPASS versions are not matching : %r != %r ' %\
-                                              (__api_version__, compass_version)
+        compass_version = version.parse(compass_package.split()[1])
+        
+        # using shesha package version
+        shesha_version = version.parse(__api_version__)
+        
+        if compass_version < shesha_version:
+            rich.print(f'[bold red]WARNING: [/bold red] SHESHA version ({shesha_version}) is higher than COMPASS version ({compass_version}).')
+            rich.print('[bold red]WARNING: [/bold red] Please update COMPASS: please run "conda update -c compass compass"')
+        elif compass_version > shesha_version:
+            rich.print(f'[bold red]WARNING: [/bold red] COMPASS version ({compass_version}) is higher than SHESHA version ({shesha_version}).')
+            rich.print('[bold red]WARNING: [/bold red] Please update SHESHA: please run "git pull origin main"')
+
+        assert(shesha_version == compass_version), f'SHESHA and COMPASS versions are not matching : {shesha_version} != {compass_version}'
 
 check_shesha_compass_versions()
